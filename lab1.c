bad0f09e39f6022850615b936846662a3036765a
setthawutleela <setthawut.leela@gmail.com>
2020-01-24 16:27:41 +0700

    lab1

---
+++ lab1.c
@@ -0,0 +1,30 @@
+/* lab1.c
+   Created by  Setthawut Leelawatthanapanit    ID: 60070503466
+               Naphatthorn Kayanthanakorn      ID: 60070503482
+*/
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int i;
+    int id;
+    int p;
+    int j = 1;    // For loop
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    
+    if(id != 0)
+        MPI_Send(&id, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    else{
+        while(j < p){
+            MPI_Recv(&i, 1, MPI_INT, j, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Hello Rank %d, I'm rank %d.\n", id, i);
+            fflush(stdin);
+            j++;
+        }
+    }
+    MPI_Finalize();
+}
