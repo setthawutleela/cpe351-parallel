e5dbf6d494325a5923dced69bd4182990d51967a
setthawutleela <49566573+setthawutleela@users.noreply.github.com>
2020-02-14 14:43:43 +0700

    Add files via upload

--- lab3.2.cpp
+++ lab3.2.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Request request[5];
 
     if (rank == 0) {
         int nrows, ncols, nrows1, ncols1, nrows2, ncols2, groupRows, remainRows;
@@ -48,8 +49,10 @@ int main(int argc, char* argv[]) {
         if (p > 1) { // The amount of process is more than 1
 
             for (int i = 1; i < p; i++) {
-                MPI_Send(&groupRows, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-                MPI_Send(&ncols, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+                //MPI_Send(&groupRows, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                //MPI_Send(&ncols, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+                MPI_Isend(&groupRows, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &request[0]);
+                MPI_Isend(&ncols, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &request[1]);
             }
 
             if (remainRows >= 1) { // Have the remaining rows after sending the tasks
@@ -57,13 +60,17 @@ int main(int argc, char* argv[]) {
             }
 
             for (int i = 1; i < p; i++) {
-                MPI_Send(&(pMat1[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 2, MPI_COMM_WORLD);
-                MPI_Send(&(pMat2[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 3, MPI_COMM_WORLD);
+                //MPI_Send(&(pMat1[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 2, MPI_COMM_WORLD);
+                //MPI_Send(&(pMat2[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 3, MPI_COMM_WORLD);
+                MPI_Isend(&(pMat1[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 2, MPI_COMM_WORLD, &request[2]);
+                MPI_Isend(&(pMat2[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 3, MPI_COMM_WORLD, &request[3]);
                 printf("Rank 0 sent the information to rank %d succesfully\n", i);
             }
 
             for (int i = 1; i < p; i++) {
-                MPI_Recv(&(pMatRes[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //MPI_Recv(&(pMatRes[groupRows * i][0]), groupRows * ncols, MPI_FLOAT, i, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Irecv(&pMatRes[groupRows * i][0], groupRows * ncols, MPI_FLOAT, i, 4, MPI_COMM_WORLD, &request[4]);
+                MPI_Wait(&request[4], MPI_STATUS_IGNORE);
             }
         }
 
@@ -87,18 +94,27 @@ int main(int argc, char* argv[]) {
         float** pMat2 = NULL;
         float** pMatRes = NULL;
 
-        MPI_Recv(&nrows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&ncols, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //MPI_Recv(&nrows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //MPI_Recv(&ncols, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Irecv(&nrows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request[0]);
+        MPI_Irecv(&ncols, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &request[1]);
+        MPI_Wait(&request[0], MPI_STATUS_IGNORE);
+        MPI_Wait(&request[1], MPI_STATUS_IGNORE);
         pMat1 = createMatrix(nrows, ncols);
         pMat2 = createMatrix(nrows, ncols);
         pMatRes = createMatrix(nrows, ncols);
 
-        MPI_Recv(&(pMat1[0][0]), nrows * ncols, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&(pMat2[0][0]), nrows * ncols, MPI_FLOAT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //MPI_Recv(&(pMat1[0][0]), nrows * ncols, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //MPI_Recv(&(pMat2[0][0]), nrows * ncols, MPI_FLOAT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Irecv(&pMat1[0][0], nrows * ncols, MPI_FLOAT, 0, 2, MPI_COMM_WORLD, &request[2]);
+        MPI_Irecv(&pMat2[0][0], nrows * ncols, MPI_FLOAT, 0, 3, MPI_COMM_WORLD, &request[3]);
+        MPI_Wait(&request[2], MPI_STATUS_IGNORE);
+        MPI_Wait(&request[3], MPI_STATUS_IGNORE);
         printf("Rank %d received the information from rank 0 successfully\n", rank);
 
         pMatRes = calAddition(pMatRes, pMat1, pMat2, 0, nrows, ncols);
-        MPI_Send(&(pMatRes[0][0]), nrows * ncols, MPI_FLOAT, 0, 4, MPI_COMM_WORLD);
+        //MPI_Send(&(pMatRes[0][0]), nrows * ncols, MPI_FLOAT, 0, 4, MPI_COMM_WORLD);
+        MPI_Isend(&pMatRes[0][0], nrows * ncols, MPI_FLOAT, 0, 4, MPI_COMM_WORLD, &request[4]);
 
         printf("Rank %d sent the result to rank 0\n", rank);
         printf("Rank %d completed\n", rank);
