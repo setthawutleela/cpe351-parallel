ca162fa9a8ec49ed3cc83f58699fa9060a1cc7ff
setthawutleela <49566573+setthawutleela@users.noreply.github.com>
2020-02-16 19:02:50 +0700

    Add files via upload

--- lab4.1.cpp
+++ lab4.1.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #define SHORTLEN 32
 
@@ -10,7 +9,7 @@
 
 int* readFile(const char* fileName, int* nrows, int* ncols, int isReverse);
 int* createMatrix(int nrows, int ncols);
-int* multipleMatrix(int* pMatRes, int* pMat1, int* pMat2, int nrows1, int ncols1, int nrows2, int ncols2, int nrowsRes, int ncolsRes);
+int* multiplyMatrix(int* pMatRes, int* pMat1, int* pMat2, int startRow, int nrows1, int ncols1, int nrows2, int ncols2, int nrowsRes, int ncolsRes);
 void printMatrix(int* pMat, int nrows, int ncols, int isReverse);
 void print1DArray(int* pMat, int size);
 void writeFile(int* pMatRes, const char* fileName, int nrows, int ncols);
@@ -18,30 +17,19 @@ void writeFile(int* pMatRes, const char* fileName, int nrows, int ncols);
 int main(int argc, char* argv[]) {
     int p;
     int rank;
+    int nrows1, ncols1, nrows2, ncols2, groupRows, remainRows;
+    int* pMat1 = NULL, * pMat2 = NULL, * pMatRes = NULL;
+    int* pMatBuf1 = NULL, * pMatBufRes = NULL;
+    double startTime, endTime;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Read file
     if (rank == 0) {
-        int nrows1, ncols1, nrows2, ncols2, nrowsRes, ncolsRes;
-        int* pMat1 = NULL;
-        int* pMat2 = NULL;
-        int* pMatRes = NULL;
-        double startTime, endTime;
-
         pMat1 = readFile("matAsmall.txt", &nrows1, &ncols1, 0);
-        printf("row1: %d | col1: %d\n", nrows1, ncols1);
         pMat2 = readFile("matBsmall.txt", &nrows2, &ncols2, 1);
-        printf("row2: %d | col2: %d\n", nrows2, ncols2);
-
-        /*printf("MATRIX A:\n");
-        print1DArray(pMat1, nrows1 * ncols1);
-        printMatrix(pMat1, nrows1, ncols1, 0);
-        printf("\n");
-        printf("MATRIX B:\n");
-        print1DArray(pMat2, nrows2 * ncols2);
-        printMatrix(pMat2, nrows2, ncols2, 1);*/
 
         // The number of rows in matrix 1 isn't equal to the number of columns in matrix 2
         if (ncols1 != nrows2) {
@@ -50,42 +38,87 @@ int main(int argc, char* argv[]) {
             exit(0);
         }
 
-        nrowsRes = nrows1;
-        ncolsRes = ncols2;
+        groupRows = nrows1/p;
+        remainRows = nrows1 - (groupRows * p);
 
-        pMatRes = createMatrix(nrowsRes, ncolsRes);
+        //printf("nrows1: %d | ncols1: %d | nrows2: %d | ncols2: %d | groupRows: %d | remainRows: %d\n", nrows1, ncols1, nrows2, ncols2, groupRows, remainRows);
 
+        pMatRes = createMatrix(nrows1, ncols1);
+    }
+
+    // Bcast the information to all ranks
+    if (p > 1) {
+        MPI_Bcast(&groupRows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&nrows1, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&ncols1, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&nrows2, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&ncols2, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        printf("Rank %d Bcast completely!\n", rank);
+    }
+
+    // Allocate memory in other ranks
+    if(rank != 0) {
+        //printf("RANK %d: \n", rank);
+        //printf("row1: %d | col1: %d | row2: %d | col2: %d\n", nrows1, ncols1, nrows2, ncols2);
+        pMatBuf1 = createMatrix(groupRows, ncols1);
+        pMat2 = createMatrix(nrows2, ncols2);
+        pMatBufRes = createMatrix(groupRows, ncols2);
+    }
+
+    if (p > 1) {
+        MPI_Bcast(&pMat2[0], nrows2 * ncols2, MPI_INT, 0, MPI_COMM_WORLD);
+        printf("Bcast pMat2 to other ranks in Rank %d\n", rank);
+    }
+
+    // Calculate in rank 0
+    if (rank == 0) {
         startTime = MPI_Wtime();
-        pMatRes = multipleMatrix(pMatRes, pMat1, pMat2, nrows1, ncols1, nrows2, ncols2, nrowsRes, ncolsRes);
+        if (p > 1) {
+            for (int i = 1; i < p; i++) {
+                MPI_Send(&(pMat1[nrows1 * i]), groupRows * ncols1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                printf("Rank %d send the information to rank %d successfully\n", rank, i);
+            }
+
+            if (remainRows >= 1) {
+                pMatRes = multiplyMatrix(pMatRes, pMat1, pMat2, groupRows * p, nrows1, ncols1, nrows2, ncols2, nrows1, ncols2);
+            }
+
+            for (int i = 1; i < p; i++) {
+                MPI_Recv(&(pMatRes[groupRows * i]), groupRows * ncols2, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+  
+        pMatRes = multiplyMatrix(pMatRes, pMat1, pMat2, 0, groupRows, ncols1, nrows2, ncols2, nrows1, ncols2);
+
         endTime = MPI_Wtime();
 
-        /*printf("\n");
-        printf("MATRIX RESULT:\n");
-        print1DArray(pMatRes, nrowsRes * ncolsRes);
-        printMatrix(pMatRes, nrowsRes, ncolsRes, 0);*/
-
         printf("Rank 0 completed.\n");
-        printf("\t====================================\n"); 
+        printf("\t====================================\n");
         printf("\t|| Calculation time: %f sec ||\n", endTime - startTime);
         printf("\t====================================\n");
-        //writeFile(pMatRes, "output.txt", nrowsRes, ncolsRes);
+
+        writeFile(pMatRes, "output.txt", nrows1, ncols2);
+        
         free(pMat1);
         free(pMat2);
         free(pMatRes);
     }
 
-    else {  // Other ranks
-        /*int nrows, ncols;
-        int* pMat1 = NULL;
-        int* pMat2 = NULL;
-        int* pMatRes = NULL;*/
+    // Calculate in other ranks
+    else {
+        MPI_Recv(&(pMatBuf1[0]), groupRows * ncols1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Rank %d received the sliced matrix from rank 0\n", rank);
 
-        /***************************
-        *      Coding here
-        ****************************/
+        pMatBufRes = multiplyMatrix(pMatBufRes, pMatBuf1, pMat2, 0, groupRows, ncols1, nrows2, ncols2, groupRows, ncols2);
 
-        /*printf("Rank %d sent the result to rank 0\n", rank);
-        printf("Rank %d completed\n", rank);*/
+        MPI_Send(&(pMatBufRes[0]), groupRows* ncols2, MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+        printf("Rank %d sent the result to rank 0\n", rank);
+        printf("Rank %d complete\n", rank);
+
+        free(pMatBuf1);
+        free(pMat2);
+        free(pMatBufRes);
     }
 
     MPI_Finalize();
@@ -140,8 +173,8 @@ int* createMatrix(int nrows, int ncols) {
     return pData;
 }
 
-int* multipleMatrix(int* pMatRes, int* pMat1, int* pMat2, int nrows1, int ncols1, int nrows2, int ncols2, int nrowsRes, int ncolsRes) {
-    for (int i = 0; i < nrows1; i++) {
+int* multiplyMatrix(int* pMatRes, int* pMat1, int* pMat2, int startRow,int nrows1, int ncols1, int nrows2, int ncols2, int nrowsRes, int ncolsRes) {
+    for (int i = startRow; i < nrows1; i++) {
         for (int j = 0; j < ncols2; j++) {
             for (int k = 0; k < nrows2; k++) {
                 //int posRes = (ncols2 * i) + j
@@ -182,4 +215,40 @@ void print1DArray(int* pMat, int size) {
         printf("%d ", pMat[i]);
     }
     printf("\n");
+}
+
+void writeFile(int* pMatRes, const char* fileName, int nrows, int ncols) {
+    FILE* fp = NULL;
+    fp = fopen(fileName, "w");
+
+    if (fp == NULL) {
+        printf("Error! opening %s file\n", fileName);
+        exit(0);
+    }
+
+    fprintf(fp, "%d %d\n", nrows, ncols);
+
+    /*for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            fprintf(fp, "%.1f", pMatRes[i][j]);
+            if (j < ncols - 1)
+                fprintf(fp, " ");
+        }
+        fprintf(fp, "\n");
+    }*/
+
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            //printf("%d", pMatRes[(i * ncols) + j]);
+            fprintf(fp, "%d", pMatRes[(i * ncols) + j]);
+            if (j < ncols - 1)
+                fprintf(fp, " ");
+        }
+        //printf("\n");
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+    printf("Write file successfully\n");
+
 }
