75150452df7ba6a3b8fe15077e567884074aa559
setthawutleela <49566573+setthawutleela@users.noreply.github.com>
2020-02-23 12:40:12 +0700

    Add files via upload

--- assignment1.cpp
+++ assignment1.cpp
@@ -71,26 +71,9 @@ int main(int argc, char* argv[]) {
         double endTime = MPI_Wtime();
         printf("Time: %lf\n", endTime - startTime);
     }
-    else if (p > 1) {
-        if (rank == 0) {
-            FILE* fp = fopen(argv[1], "r");;
-            int i = 0, j, nrows, ncols;
-            fscanf(fp, "%d %d", &nrows, &ncols);
-            float* pMat = (float*)calloc(nrows * ncols, sizeof(float));
-            float* pMatRes = (float*)calloc(nrows * ncols, sizeof(float));
-            while (fscanf(fp, "%f", &pMat[i++]) == 1);
-            fclose(fp);
-            MPI_Bcast(&ncols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            int groupRows = (nrows - 2) / p;
-            int remainRows = (nrows - 2) - ((nrows - 2) / p) * p;
-            printf("groupRows = %d\n", groupRows);
-            printf("remainRows = %d\n", remainRows);
-            MPI_Bcast(&groupRows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        }
-    }
 
-    /*else if (rank == 0) {
-        FILE* fp = fopen(argv[1], "r");;
+    else if (rank == 0) {
+        FILE* fp = fopen(argv[1], "r");
         int i = 0, j, nrows, ncols;
         fscanf(fp, "%d %d", &nrows, &ncols);
         float* pMat = (float*)calloc(nrows * ncols, sizeof(float));
@@ -103,12 +86,12 @@ int main(int argc, char* argv[]) {
         printf("groupRows = %d\n", groupRows);
         printf("remainRows = %d\n", remainRows);
         MPI_Bcast(&groupRows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        //printf("RANK %d\n", rank);
         int loop = 0;
         while (loop < atoi(argv[3])) {
             for (i = 1; i < p; i++) {
                 int rowPos = i * groupRows * ncols;
-                MPI_Send(&(pMat[rowPos]), (groupRows + 2) * ncols, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+                MPI_Isend(&(pMat[rowPos]), (groupRows + 2) * ncols, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &sRequest);
+                MPI_Wait(&sRequest, MPI_STATUS_IGNORE);
             }
             i = 2;
             while (i < groupRows + 2) {
@@ -177,37 +160,40 @@ int main(int argc, char* argv[]) {
         float* pMat = NULL, * pMatBuf = NULL;
         MPI_Bcast(&ncols, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(&groupRows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        //printf("RANK %d\n", rank);
         pMatBuf = (float*)calloc((groupRows + 2) * ncols, sizeof(float));
         float* pMatRes = (float*)calloc((groupRows + 2) * ncols, sizeof(float));
-        MPI_Recv(&(pMatBuf[0]), (groupRows+2) * ncols, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        i = 2;
-        while (i < groupRows + 2) {
-            int posRes = (i * ncols) - 2;
-            int posA = ((i - 1) * ncols) - 3;
-            int posB = ((i - 1) * ncols) - 2;
-            int posC = ((i - 1) * ncols) - 1;
-            int posD = (i * ncols) - 3;
-            int posE = (i * ncols) - 1;
-            int posF = ((i + 1) * ncols) - 3;
-            int posG = ((i + 1) * ncols) - 2;
-            int posH = ((i + 1) * ncols) - 1;
-            for (int k = posRes; k > (posRes - ncols) + 2; k--) {
-                pMatRes[k] = (pMatBuf[posA--] + pMatBuf[posB--] + pMatBuf[posC--] + pMatBuf[posD--] + pMatBuf[posE--] + pMatBuf[posF--] + pMatBuf[posG--] + pMatBuf[posH--] + pMatBuf[k]) / 9;
-                //printf("%.f\n", pMatRes[k]);
+        int loop = 0;
+        while (loop < atoi(argv[3])) {
+            MPI_Irecv(&(pMatBuf[0]), (groupRows + 2) * ncols, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &rRequest[rank]);
+            MPI_Wait(&rRequest[rank], MPI_STATUS_IGNORE);
+            i = 2;
+            while (i < groupRows + 2) {
+                int posRes = (i * ncols) - 2;
+                int posA = ((i - 1) * ncols) - 3;
+                int posB = ((i - 1) * ncols) - 2;
+                int posC = ((i - 1) * ncols) - 1;
+                int posD = (i * ncols) - 3;
+                int posE = (i * ncols) - 1;
+                int posF = ((i + 1) * ncols) - 3;
+                int posG = ((i + 1) * ncols) - 2;
+                int posH = ((i + 1) * ncols) - 1;
+                for (int k = posRes; k > (posRes - ncols) + 2; k--) {
+                    pMatRes[k] = (pMatBuf[posA--] + pMatBuf[posB--] + pMatBuf[posC--] + pMatBuf[posD--] + pMatBuf[posE--] + pMatBuf[posF--] + pMatBuf[posG--] + pMatBuf[posH--] + pMatBuf[k]) / 9;
+                }
+                i++;
             }
-            i++;
-        }
 
-        i = 2;
-        while (i < groupRows+2) {
-            int posRes = (i * ncols) - 2;
-            for (int k = posRes; k > (posRes - ncols) + 2; k--) {
-                pMatBuf[k] = pMatRes[k];
+            i = 2;
+            while (i < groupRows + 2) {
+                int posRes = (i * ncols) - 2;
+                for (int k = posRes; k > (posRes - ncols) + 2; k--) {
+                    pMatBuf[k] = pMatRes[k];
+                }
+                i++;
             }
-            i++;
+            MPI_Isend(&(pMatBuf[ncols]), groupRows * ncols, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &sRequest);
+            loop++;
         }
-        MPI_Isend(&(pMatBuf[ncols]), groupRows*ncols, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &sRequest);
-    }*/
+    }
     MPI_Finalize();
 }
